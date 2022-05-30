1a514741b6130f03683268c18131abaa20d494a5
MedBOU7A <99727708+MedBOU7A@users.noreply.github.com>
2022-05-30 18:38:58 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { hasPointerEvents } from '@testing-library/user-event/dist/utils'
+import React, { Component } from 'react'
+import image from './img/2.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state ={
+    FullName : "Moamed Amine Bouhari",
+    Bio :"26/10/1992",
+    Profession :"Web developper",
+    visible : true,
+    count : 0,
+    }
+   increment =()=> this.setState({count : this.state.count +1})
+   decrement =()=> this.setState({count : this.state.count -1})
+   handelShow =()=>this.setState({visible : !this.state.visible})
+
+  render() {
+    return (
+     <div>
+       <button onClick={this.handelShow}>{this.state.visible ? 'show' :'hide'}</button>
+       { this.state.visible &&
+      <div>
+        <h1>counter :{this.state.count}</h1>
+        <button onClick={this.increment}> + </button>
+        <button onClick={this.decrement}> - </button>
+        <h1>My name is : {this.state.FullName}</h1>
+        <h1>Bio : {this.state.Bio}</h1>
+        <h1>Profession :{this.state.Profession}</h1>
+        <img src={image} alt ='image' />
+      </div>
+       }
+     </div>
+    )
+  }
 }
-
-export default App;
